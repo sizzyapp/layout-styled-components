b34e8a0feee9878851c868c545d1f4141e4edbe5
Kristijan Ristovski <kristijan.mkd@gmail.com>
2019-08-03 18:20:48 +0200

    initial

---
+++ src/flex-components.ts
@@ -0,0 +1,95 @@
+import styled from 'styled-components';
+import flex from './flex';
+
+const margin = {
+  horizontal: 'marginRight',
+  vertical: 'marginBottom'
+};
+
+interface CommonProps {
+  wrap?: boolean;
+  spaceBetween?: boolean;
+  spaceAround?: boolean;
+  justifyEnd?: boolean;
+  justifyStart?: boolean;
+  flex?: number;
+  spaceFirst?: boolean;
+  spaceAll?: number;
+  spaceBottom?: boolean;
+  alignStart?: boolean;
+  alignItemsStart?: boolean;
+  center?: boolean;
+  centerV?: boolean;
+  centerH?: boolean;
+  noShrink?: boolean;
+  styles?: any;
+  invert?: boolean;
+}
+
+export const common = (direction: 'horizontal' | 'vertical') => (
+  p: CommonProps
+) => ({
+  ...(p.spaceBetween && flex.spaceBetween),
+  ...(p.wrap && flex.wrap),
+  ...(p.spaceAround && flex.spaceAround),
+  ...(p.justifyEnd && flex.justifyEnd),
+  ...(p.justifyStart && flex.justifyStart),
+  ...(p.alignStart && flex.alignStart),
+  ...(p.alignItemsStart && flex.alignItemsStart),
+  ...(p.flex && { flex: p.flex }),
+  ...(p.spaceFirst && {
+    '& :first-child': { [margin[direction]]: p.spaceFirst }
+  }),
+  ...(p.spaceAll && {
+    '& > *': {
+      [margin[direction]]: p.spaceAll,
+      ...(p.spaceBottom && { marginBottom: p.spaceAll })
+    },
+    '& > *:last-child': {
+      [margin[direction]]: 0,
+      ...(p.spaceBottom && { marginBottom: 0 })
+    }
+  }),
+  ...(p.noShrink && {
+    flexShrink: 0
+  }),
+  ...(p.styles && p.styles)
+});
+
+const horizontalProps = (props:any) => {
+  const { center, centerV, centerH } = props;
+  return {
+    ...flex.horizontal,
+    ...(center && flex.centerHorizontal),
+    ...(centerV && flex.centerHorizontalV),
+    ...(centerH && flex.centerHorizontalH),
+    ...common('horizontal')(props)
+  };
+};
+
+const verticalProps = (props:any) => {
+  const { center, centerV, centerH } = props;
+  return {
+    ...flex.vertical,
+    ...(center && flex.centerVertical),
+    ...(centerV && flex.centerVerticalV),
+    ...(centerH && flex.centerVerticalH),
+    ...common('vertical')(props)
+  };
+};
+
+const getHorizontalProps = ({ invert = false, ...rest }) => {
+  return invert === false ? horizontalProps(rest) : verticalProps(rest);
+};
+
+const getVerticalProps = ({ invert = false, ...rest }) => {
+  return invert === false ? verticalProps(rest) : horizontalProps(rest);
+};
+
+export const Horizontal = styled.div<CommonProps>(getHorizontalProps);
+export const Vertical = styled.div<CommonProps>(getVerticalProps);
+
+export const TopFlex = styled.div({
+  ...flex.vertical,
+  flex: 1
+});
---
+++ src/flex.ts
@@ -0,0 +1,72 @@
+import { CSSObject } from 'styled-components';
+
+interface CSSObjects {
+  [propertyName: string]: CSSObject;
+}
+
+export const flex: CSSObjects = {
+  horizontal: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  vertical: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  flexWrap: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  centerHorizontal: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  wrap: {
+    flexWrap: 'wrap'
+  },
+  noWrap: {
+    flexWrap: 'nowrap'
+  },
+  centerHorizontalH: {
+    justifyContent: 'center'
+  },
+  centerHorizontalV: {
+    alignItems: 'center'
+  },
+  centerVertical: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  centerVerticalH: {
+    alignItems: 'center'
+  },
+  centerVerticalV: {
+    justifyContent: 'center'
+  },
+  spaceAround: {
+    justifyContent: 'space-around'
+  },
+  spaceBetween: {
+    justifyContent: 'space-between'
+  },
+  justifyEnd: {
+    justifyContent: 'flex-end'
+  },
+  justifyStart: {
+    justifyContent: 'flex-start'
+  },
+  alignStart: {
+    alignContent: 'flex-start'
+  },
+  stretchSelf: {
+    alignSelf: 'stretch'
+  },
+  alignItemsStart:{
+    alignItems: 'flex-start'
+  },
+  selfEnd: {
+    alignSelf: 'flex-end'
+  }
+};
+
+export default flex;
